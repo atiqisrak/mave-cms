38dd7fc7ee78ee6ac28632b0261ea14e4c226f17
atiqisrak <atiq.israk@webable.digital>
2024-01-15 20:58:42 +0600

    formparser edit working

--- components/creator/SliderParser.js
+++ components/creator/SliderParser.js
@@ -34,7 +34,7 @@ const SliderParser = ({ item, editMode, onSliderSelect }) => {
       const response = await instance("/sliders");
       if (response.data) {
         setSliders(response.data);
-        console.log("Sliders: ", response.data);
+        console.log("Sliders: ", sliders);
         setLoading(false);
       } else {
         message.error("Error fetching sliders");
@@ -101,7 +101,7 @@ const SliderParser = ({ item, editMode, onSliderSelect }) => {
             onChange={handleSliderChange}
           >
             {sliders?.map((slider) => (
-              <Option value={slider?.id}>{slider?.title_en}</Option>
+              <Option value={slider?.id}>{slider?._mave?.title_en}</Option>
             ))}
           </Select>
         </div>
