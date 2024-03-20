5ce6bd8e1367580a67169599f347eddb8d0c2116
atiqisrak <atiq.israk@webable.digital>
2024-03-20 13:57:08 +0600

    updated nextconfig

--- next.config.js
+++ next.config.js
@@ -1,5 +1,16 @@
-module.exports = {
+const nextConfig = {
+  dangerouslyAllowSVG: true,
+  distDir: ".next",
+  output: "out",
+  eslint: {
+    dirs: ["."],
+    ignoreDuringBuilds: true,
+  },
   images: {
+    reactStrictMode: true,
     domains: ["mave-almuslim.etherstaging.xyz"],
+    unoptimized: true,
   },
 };
+
+module.exports = nextConfig;
