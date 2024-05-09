a561506a1f96b0fe6b9cbadec8ff150578e09e89
atiqisrak <atiq.israk@webable.digital>
2024-05-09 15:12:11 +0600

    slider updated

--- pages/sliders.js
+++ pages/sliders.js
@@ -9,6 +9,7 @@ import {
   Popconfirm,
   Radio,
   Row,
+  Segmented,
   Select,
   Space,
   Tabs,
@@ -32,10 +33,14 @@ import { useRouter } from "next/router";
 import MediaSelectionModal from "../components/MediaSelectionModal";
 import CreateSliderComponent from "../components/CreateSliderComponent";
 import RichTextEditor from "../components/RichTextEditor";
+import ImageSliders from "../components/ImageSliders";
+import CardSliders from "../components/CardSliders";
 
 const Sliders = () => {
   const [isModalVisible, setIsModalVisible] = useState(false);
   const [sliders, setSliders] = useState([]);
+  const [imageSliders, setImageSliders] = useState([]);
+  const [cardSliders, setCardSliders] = useState([]);
   const [loading, setLoading] = useState(false);
   const { Title } = Typography;
   const [selectedMedia, setSelectedMedia] = useState([]);
@@ -47,7 +52,7 @@ const Sliders = () => {
   const [showCreateSliderForm, setShowCreateSliderForm] = useState(false);
   const MEDIA_URL = process.env.NEXT_PUBLIC_MEDIA_URL;
   const [cards, setCards] = useState([]);
-  const [sliderType, setSliderType] = useState("image");
+  const [type, setType] = useState("image");
 
   const CustomPrevArrow = ({ onClick }) => (
     <Button
@@ -85,19 +90,22 @@ const Sliders = () => {
     const fetchSliders = async () => {
       try {
         setLoading(true);
-        // const response = await axios.get(`${API_BASE_URL}/media`);
         const response = await instance("/sliders");
         if (response.data) {
           setSliders(response.data?.sort((a, b) => b.id - a.id));
-          // console.log("Media Assets: ", response.data);
-          // message.success("Sliders fetched successfully");
+
+          response.data?.map((slider) => {
+            if (slider.type === "card") {
+              setCardSliders((prev) => [...prev, slider]);
+            } else {
+              setImageSliders((prev) => [...prev, slider]);
+            }
+          });
           setLoading(false);
         } else {
-          // console.error("Error fetching media assets:", response.data.message);
           message.error("Sliders couldn't be fetched");
         }
       } catch (error) {
-        // console.error("Error fetching media assets:", error);
         message.error("Sliders couldn't be fetched");
       }
     };
@@ -108,21 +116,19 @@ const Sliders = () => {
         const response = await instance.get("/cards");
         if (response.data) {
           setCards(response.data);
-          // console.log("Cards: ", response.data);
           message.success("Cards fetched successfully");
           setLoading(false);
         } else {
-          // console.error("Error fetching cards:", response.data.message);
           message.error("Cards couldn't be fetched");
         }
       } catch (error) {
-        // console.error("Error fetching cards:", error);
         message.error("Cards couldn't be fetched");
       }
     };
     fetchCards();
     fetchSliders();
   }, [responseData, response]);
+
   const showModal = () => {
     setIsModalVisible(true);
     // router.push("/create-slider");
@@ -140,7 +146,6 @@ const Sliders = () => {
   };
 
   const handDeleteSlider = async (e, id) => {
-    // console.log("your log output", id);
     try {
       setLoading(true);
       const response = await instance.delete(`/sliders/${id}`);
@@ -180,9 +185,9 @@ const Sliders = () => {
     const previousDescriptionBn = sliders.find(
       (slider) => slider.id === editingItemId
     ).description_bn;
-    const previousSliderType = sliders.find(
+    const previousType = sliders.find(
       (slider) => slider.id === editingItemId
-    ).slider_type;
+    ).type;
     const previousCardsIds = sliders.find(
       (slider) => slider.id === editingItemId
     ).card_ids;
@@ -200,13 +205,10 @@ const Sliders = () => {
         description_bn: values.description_bn
           ? values.description_bn
           : previousDescriptionBn,
-        slider_type: values.slider_type
-          ? values.slider_type
-          : previousSliderType,
+        type: values.type ? values.type : previousType,
         card_ids: values.card_ids ? values.card_ids : previousCardsIds,
         media_ids: selectedMedia?.length > 0 ? selectedMedia : previousMediaIds,
       };
-      console.log("Sending data: ", postData);
       const response = await instance.put(
         `/sliders/${editingItemId}`,
         postData
@@ -223,6 +225,7 @@ const Sliders = () => {
         setEditingItemId(null);
         setSelectedMedia([]);
         setLoading(false);
+        window.location.reload();
       } else {
         console.error("Error creating slider:", response.data);
         setLoading(false);
@@ -280,225 +283,71 @@ const Sliders = () => {
               <Loader />
             ) : (
               <>
-                <Row gutter={{ xs: 8, sm: 16, md: 24, lg: 32 }}>
-                  {sliders?.map((asset) => (
-                    <Col span={12} style={{ marginTop: "1rem" }} key={asset.id}>
-                      {editingItemId === asset.id ? (
-                        <Form
-                          form={form}
-                          name="createSlider"
-                          onFinish={handleSubmit}
-                          style={{
-                            marginTop: "10em",
-                            maxWidth: 600,
-                            margin: "0 auto",
-                          }}
-                          layout="vertical"
-                          autoComplete="off"
-                        >
-                          <Form.Item
-                            hasFeedback
-                            label="Title English"
-                            name="title_e"
-                          >
-                            <Input
-                              placeholder="Enter title in English"
-                              defaultValue={asset.title_en}
-                            />
-                          </Form.Item>
-                          <Form.Item
-                            hasFeedback
-                            label="Title Bangla"
-                            name="title_b"
-                          >
-                            <Input
-                              placeholder="Enter title in Bangla"
-                              defaultValue={asset.title_bn}
-                            />
-                          </Form.Item>
-                          <Form.Item
-                            hasFeedback
-                            label="Description English"
-                            name="description_en"
-                          >
-                            <RichTextEditor
-                              value={asset.description_en}
-                              defaultValue={asset.description_en}
-                              editMode={true}
-                              onChange={(value) =>
-                                form.setFieldsValue({ description_en: value })
-                              }
-                            />
-                          </Form.Item>
-                          <Form.Item
-                            hasFeedback
-                            label="Description Bangla"
-                            name="description_bn"
-                          >
-                            <RichTextEditor
-                              value={asset.description_bn}
-                              defaultValue={asset.description_bn}
-                              editMode={true}
-                              onChange={(value) =>
-                                form.setFieldsValue({ description_bn: value })
-                              }
-                            />
-                          </Form.Item>
-                          <Form.Item
-                            hasFeedback
-                            label="Slider type"
-                            name="slider_type"
-                          >
-                            <Tabs
-                              defaultActiveKey="image"
-                              onChange={(key) => setSliderType(key)}
-                            >
-                              <Tabs.TabPane
-                                tab="Image"
-                                key="image"
-                              ></Tabs.TabPane>
-                              <Tabs.TabPane
-                                tab="Card"
-                                key="card"
-                              ></Tabs.TabPane>
-                            </Tabs>
-                          </Form.Item>
-                          {sliderType === "card" ? (
-                            <Form.Item
-                              hasFeedback
-                              label="Select Card"
-                              name="card_ids"
-                            >
-                              <Select
-                                mode="multiple"
-                                placeholder="Select a card"
-                                style={{ width: "100%" }}
-                              >
-                                {cards.map((card) => (
-                                  <Select.Option value={card.id} key={card.id}>
-                                    {card.title_en}
-                                  </Select.Option>
-                                ))}
-                              </Select>
-                            </Form.Item>
-                          ) : (
-                            <Form.Item hasFeedback label="Media" name="title_m">
-                              <Button
-                                icon={<UploadOutlined />}
-                                onClick={showModal}
-                              >
-                                Click to Select
-                              </Button>
-                            </Form.Item>
-                          )}
-                          <Form.Item>
-                            <Space>
-                              <Button type="primary" htmlType="submit">
-                                Submit
-                              </Button>
-                              <Button
-                                onClick={() => handleCancelEdit()}
-                                type="primary"
-                                danger
-                              >
-                                Cancel
-                              </Button>
-                            </Space>
-                            <Modal
-                              width={"70%"}
-                              title="Upload Media"
-                              open={isModalVisible}
-                              onOk={handleOk}
-                              onCancel={handleCancel}
-                              className="uploadMediaModal"
-                            >
-                              <MediaSelectionModal
-                                currentMedia={asset?.medias}
-                                selectedMedia={selectedMedia}
-                                setSelectedMedia={setSelectedMedia}
-                                isModalVisible={isModalVisible}
-                                setIsModalVisible={setIsModalVisible}
-                              />
-                            </Modal>
-                          </Form.Item>
-                        </Form>
-                      ) : (
-                        <div
-                          style={{
-                            marginBottom: "5rem",
-                            border: "1px solid #f0f0f0",
-                            borderRadius: "10px",
-                            padding: "2.4rem",
-                            boxShadow: "0 0 10px rgba(0,0,0,0.1)",
-                          }}
-                        >
-                          <Carousel
-                            style={{ position: "relative" }}
-                            autoplay
-                            arrows={true}
-                            prevArrow={<CustomPrevArrow />}
-                            nextArrow={<CustomNextArrow />}
-                          >
-                            {asset?.medias?.map((img, index) => (
-                              <div key={index}>
-                                <Image
-                                  src={`${MEDIA_URL}/${img?.file_path}`}
-                                  alt={asset.file_name}
-                                  width={"100%"}
-                                  height={400}
-                                  style={{
-                                    objectFit: "cover",
-                                    borderRadius: 10,
-                                  }}
-                                />
-                              </div>
-                            ))}
-                          </Carousel>
-
-                          <Space
-                            style={{
-                              display: "flex",
-                              flexDirection: "column",
-                              alignItems: "flex-start",
-                            }}
-                          >
-                            <Title level={4}>Title: {asset.title_en}</Title>
-                            <Title level={5}>শিরোনাম: {asset.title_bn}</Title>
-                          </Space>
-                          <Space
-                            style={{
-                              marginTop: "1em",
-                              display: "flex",
-                              justifyContent: "center",
-                              width: "100%",
-                            }}
-                          >
-                            <Button
-                              onClick={() => handleEditClick(asset.id)}
-                              style={{
-                                backgroundColor: "var(--theme)",
-                                borderColor: "var(--theme)",
-                                color: "white",
-                              }}
-                            >
-                              <EditOutlined /> Edit
-                            </Button>
-                            <Popconfirm
-                              title="Are you sure delete this slider?"
-                              okText="Yes"
-                              cancelText="No"
-                              onConfirm={(e) => handDeleteSlider(e, asset?.id)}
-                            >
-                              <Button danger>
-                                <DeleteOutlined /> Delete
-                              </Button>
-                            </Popconfirm>
-                          </Space>
-                        </div>
-                      )}
-                    </Col>
-                  ))}
-                </Row>
+                <>
+                  {/* <Segmented defaultValue="1"> */}
+                  <Tabs
+                    defaultActiveKey="1"
+                    type="card"
+                    animated
+                    centered
+                    onChange={(key) => {
+                      console.log(key);
+                    }}
+                    style={{
+                      marginTop: "2rem",
+                    }}
+                  >
+                    <Tabs.TabPane tab="Image Sliders" key="1">
+                      <ImageSliders
+                        sliders={imageSliders}
+                        CustomNextArrow={CustomNextArrow}
+                        CustomPrevArrow={CustomPrevArrow}
+                        MEDIA_URL={MEDIA_URL}
+                        handleEditClick={handleEditClick}
+                        handleCancelEdit={handleCancelEdit}
+                        handDeleteSlider={handDeleteSlider}
+                        editingItemId={editingItemId}
+                        handleSubmit={handleSubmit}
+                        form={form}
+                        cards={cards}
+                        showModal={showModal}
+                        isModalVisible={isModalVisible}
+                        setIsModalVisible={setIsModalVisible}
+                        handleOk={handleOk}
+                        handleCancel={handleCancel}
+                        selectedMedia={selectedMedia}
+                        setSelectedMedia={setSelectedMedia}
+                        setType={setType}
+                        type={type}
+                      />
+                    </Tabs.TabPane>
+                    <Tabs.TabPane tab="Card Sliders" key="2">
+                      <CardSliders
+                        sliders={cardSliders}
+                        CustomNextArrow={CustomNextArrow}
+                        CustomPrevArrow={CustomPrevArrow}
+                        MEDIA_URL={MEDIA_URL}
+                        handleEditClick={handleEditClick}
+                        handleCancelEdit={handleCancelEdit}
+                        handDeleteSlider={handDeleteSlider}
+                        editingItemId={editingItemId}
+                        handleSubmit={handleSubmit}
+                        form={form}
+                        cards={cards}
+                        showModal={showModal}
+                        isModalVisible={isModalVisible}
+                        setIsModalVisible={setIsModalVisible}
+                        handleOk={handleOk}
+                        handleCancel={handleCancel}
+                        selectedMedia={selectedMedia}
+                        setSelectedMedia={setSelectedMedia}
+                        setType={setType}
+                        type={type}
+                      />
+                    </Tabs.TabPane>
+                  </Tabs>
+                  {/* </Segmented> */}
+                </>
               </>
             )}
           </div>
