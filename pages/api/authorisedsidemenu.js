3b2b9d3429df553c48728bac7a4fc97c48e3ec8f
atiqisrak <atiq.israk@webable.digital>
2024-06-02 18:36:37 +0600

    dockerized project

--- pages/api/authorisedsidemenu.js
+++
@@ -1,5 +0,0 @@
-const authorisedSideMenu = require("./authorisedsidemenus.json");
-
-export default function handler(req, res) {
-  res.status(200).json(authorisedSideMenu);
-}
--- pages/api/unauthorisedsidemenu.js
+++
@@ -1,5 +0,0 @@
-const unauthorisedsidemenu = require("./unauthorisedsidemenu.json");
-
-export default function handler(req, res) {
-  res.status(200).json(unauthorisedsidemenu);
-}
