ef17206f8bf3a4844830c2ae0efb263c70b5fc8a
Niloy <atiqisrak@gmail.com>
2024-07-30 12:25:07 +0600

    working on dnd

--- pages/formbuilder/index.js
+++ pages/formbuilder/index.js
@@ -1,25 +1,55 @@
 import { Button } from "antd";
 import router from "next/router";
+import MaveFormsList from "../../components/formbuilder/MaveFormsList";
 
 export default function FormBuilder() {
   return (
     <div className="ViewContainer">
-      <h1>Welcome to the Mave Form Builder</h1>
-      <Button
-        onClick={() => {
-          router.push("/formbuilder/create-form");
-        }}
-      >
-        Create Form
-      </Button>
+      <center>
+        <h1>Welcome to the Mave Form Builder</h1>
+      </center>
 
-      <Button
-        onClick={() => {
-          router.push("/formbuilder/mave-forms-showcase");
+      <div
+        style={{
+          display: "flex",
+          justifyContent: "center",
+          gap: "2rem",
+          marginTop: "2rem",
         }}
       >
-        View Forms
-      </Button>
+        <Button
+          style={{
+            backgroundColor: "var(--themes)",
+            color: "white",
+            fontSize: "1.2rem",
+            fontWeight: "bold",
+            padding: "1.5rem 2rem",
+          }}
+          onClick={() => {
+            router.push("/formbuilder/create-form");
+          }}
+        >
+          Create Form
+        </Button>
+
+        <Button
+          style={{
+            backgroundColor: "var(--theme)",
+            color: "white",
+            fontSize: "1.2rem",
+            fontWeight: "bold",
+            padding: "1.5rem 2rem",
+          }}
+          onClick={() => {
+            router.push("/formbuilder/mave-forms-showcase");
+          }}
+        >
+          View Forms
+        </Button>
+      </div>
+      <div>
+        <MaveFormsList />
+      </div>
     </div>
   );
 }
