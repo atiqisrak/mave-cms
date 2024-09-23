de296b98d0cefd889669aea90530873ff4dac939
atiqisrak <atiq.israk@webable.digital>
2024-09-23 15:00:32 +0600

    updated faq

--- pages/usermanual/faq.js
+++ pages/usermanual/faq.js
@@ -1,13 +1,15 @@
 import {
   ArrowRightOutlined,
+  HomeOutlined,
   RightOutlined,
   SearchOutlined,
 } from "@ant-design/icons";
-import { Collapse, Input, Select } from "antd";
+import { Layout, Collapse, Input, Select, Breadcrumb, Button } from "antd";
 import React, { useState, useEffect } from "react";
 import faqjson from "./faq.json";
 
 const faq = () => {
+  const { Content } = Layout;
   const [faqData, setFaqData] = useState([]);
   const [questions, setQuestions] = useState([]);
   const [answers, setAnswers] = useState([]);
@@ -48,176 +50,216 @@ const faq = () => {
   };
 
   return (
-    <div className="ViewContainer">
-      <div className="ViewContentContainer">
-        <div
+    <div className="ViewContainer ViewContentContainer">
+      <Layout
+        style={{
+          padding: "0 24px 24px",
+          marginBottom: "2rem",
+          backgroundColor: "transparent",
+        }}
+      >
+        <Breadcrumb
           style={{
-            display: "grid",
-            gridTemplateColumns: "1fr 3fr",
-            gap: "1em",
-            width: "100%",
+            margin: "16px 0",
+            fontSize: "1rem",
+            fontWeight: 600,
           }}
         >
-          <div
-            className="faq-categories"
+          <Breadcrumb.Item>
+            <Button icon={<HomeOutlined />} type="link" href="/" />
+          </Breadcrumb.Item>
+          <Breadcrumb.Item>User Manual</Breadcrumb.Item>
+          <Breadcrumb.Item>Frequently Asked Questions</Breadcrumb.Item>
+        </Breadcrumb>
+
+        <Content
+          style={{
+            padding: "24px",
+            margin: 0,
+            minHeight: 280,
+            backgroundColor: "#fff",
+            borderRadius: "8px",
+          }}
+        >
+          <h1
             style={{
-              borderRight: "1px solid #f0f0f0",
-              padding: "2em 1em",
-              backgroundColor: "#f9f9f9",
-              borderRadius: "10px",
+              fontSize: "1.8rem",
+              fontWeight: 600,
+              marginBottom: "1.5rem",
+              textAlign: "center",
             }}
           >
-            <Input
-              allowClear
-              suffix={<SearchOutlined />}
-              placeholder="Search for a question..."
-              style={{ width: "100%" }}
-              onChange={(e) => onSearch(e.target.value)}
-            />
+            Content Management Settings
+          </h1>
+          <div
+            style={{
+              display: "grid",
+              gridTemplateColumns: "1fr 3fr",
+              gap: "1em",
+              width: "100%",
+            }}
+          >
             <div
+              className="faq-categories"
               style={{
-                listStyleType: "none",
-                padding: "0",
-                margin: "0",
-                marginTop: "1em",
+                borderRight: "1px solid #f0f0f0",
+                padding: "2em 1em",
+                backgroundColor: "#f9f9f9",
+                borderRadius: "10px",
+              }}
+            >
+              <Input
+                allowClear
+                suffix={<SearchOutlined />}
+                placeholder="Search for a question..."
+                style={{ width: "100%" }}
+                onChange={(e) => onSearch(e.target.value)}
+              />
+              <div
+                style={{
+                  listStyleType: "none",
+                  padding: "0",
+                  margin: "0",
+                  marginTop: "1em",
+                  display: "flex",
+                  flexDirection: "column",
+                  gap: "1em",
+                }}
+              >
+                {faqData &&
+                  faqData?.map((item, index) => (
+                    <div
+                      key={index}
+                      style={{
+                        display: "flex",
+                        gap: "0.5em",
+                        padding: "1em",
+                        cursor: "pointer",
+                        fontWeight: activeFaq === item?.id ? "bold" : "",
+                        color: activeFaq === item?.id ? "white" : "",
+                        backgroundColor:
+                          activeFaq === item?.id ? "var(--theme)" : "",
+                        borderRadius: "5px",
+                      }}
+                      onClick={() => {
+                        setActiveFaq(item?.id);
+                      }}
+                    >
+                      {item?.name}
+                      {activeFaq === item?.id && <RightOutlined />}
+                    </div>
+                  ))}
+              </div>
+            </div>
+            <div
+              className="faq-questions"
+              style={{
                 display: "flex",
                 flexDirection: "column",
                 gap: "1em",
+                padding: "1em 3em",
               }}
             >
-              {faqData &&
-                faqData?.map((item, index) => (
-                  <div
-                    key={index}
-                    style={{
-                      display: "flex",
-                      gap: "0.5em",
-                      padding: "1em",
-                      cursor: "pointer",
-                      fontWeight: activeFaq === item?.id ? "bold" : "",
-                      color: activeFaq === item?.id ? "white" : "",
-                      backgroundColor:
-                        activeFaq === item?.id ? "var(--theme)" : "",
-                      borderRadius: "5px",
-                    }}
-                    onClick={() => {
-                      setActiveFaq(item?.id);
-                    }}
-                  >
-                    {item?.name}
-                    {activeFaq === item?.id && <RightOutlined />}
-                  </div>
-                ))}
-            </div>
-          </div>
-          <div
-            className="faq-questions"
-            style={{
-              display: "flex",
-              flexDirection: "column",
-              gap: "1em",
-              padding: "1em 3em",
-            }}
-          >
-            <div className="header flexed-between">
-              {activeFaq && searchMode ? (
-                <h2>Search Results for "{search}"</h2>
-              ) : (
-                faqData?.map(
-                  (item) => item?.id === activeFaq && <h1>{item?.name}</h1>
-                )
-              )}
-            </div>
-            {searchMode ? (
-              <>
-                <div className="questions">
-                  {searchResults &&
-                    searchResults?.map((item, index) => (
-                      <div key={index} className="question">
-                        <Collapse
-                          defaultActiveKey={["0"]}
-                          ghost
-                          expandIconPosition="right"
-                          style={{
-                            fontSize: "1.15em",
-                          }}
-                        >
-                          <Collapse.Panel
-                            header={item?.content?.question}
-                            key={index}
+              <div className="header flexed-between">
+                {activeFaq && searchMode ? (
+                  <h2>Search Results for "{search}"</h2>
+                ) : (
+                  faqData?.map(
+                    (item) => item?.id === activeFaq && <h1>{item?.name}</h1>
+                  )
+                )}
+              </div>
+              {searchMode ? (
+                <>
+                  <div className="questions">
+                    {searchResults &&
+                      searchResults?.map((item, index) => (
+                        <div key={index} className="question">
+                          <Collapse
+                            defaultActiveKey={["0"]}
+                            ghost
+                            expandIconPosition="right"
                             style={{
-                              borderRadius: "5px",
-                              border: "1px solid #f0f0f0",
-                              padding: "1em",
-                              marginBottom: "1em",
+                              fontSize: "1.15em",
                             }}
                           >
-                            <p
-                              dangerouslySetInnerHTML={{
-                                __html: item?.content?.answer,
-                              }}
-                            />
-                          </Collapse.Panel>
-                        </Collapse>
-                      </div>
-                    ))}
-                </div>
-              </>
-            ) : (
-              <>
-                <p
-                  style={{
-                    padding: "1em",
-                    color: "gray",
-                    borderRadius: "5px",
-                  }}
-                >
-                  {faqData &&
-                    faqData?.map(
-                      (item) => item?.id === activeFaq && item?.description
-                    )}
-                </p>
-                <div className="questions">
-                  {faqData &&
-                    faqData?.map(
-                      (item) =>
-                        item?.id === activeFaq &&
-                        item?.content?.map((content, index) => (
-                          <div key={index} className="question">
-                            <Collapse
-                              defaultActiveKey={["0"]}
-                              ghost
-                              expandIconPosition="right"
+                            <Collapse.Panel
+                              header={item?.content?.question}
+                              key={index}
                               style={{
-                                fontSize: "1.15em",
+                                borderRadius: "5px",
+                                border: "1px solid #f0f0f0",
+                                padding: "1em",
+                                marginBottom: "1em",
                               }}
                             >
-                              <Collapse.Panel
-                                header={content?.question}
-                                key={index}
+                              <p
+                                dangerouslySetInnerHTML={{
+                                  __html: item?.content?.answer,
+                                }}
+                              />
+                            </Collapse.Panel>
+                          </Collapse>
+                        </div>
+                      ))}
+                  </div>
+                </>
+              ) : (
+                <>
+                  <p
+                    style={{
+                      padding: "1em",
+                      color: "gray",
+                      borderRadius: "5px",
+                    }}
+                  >
+                    {faqData &&
+                      faqData?.map(
+                        (item) => item?.id === activeFaq && item?.description
+                      )}
+                  </p>
+                  <div className="questions">
+                    {faqData &&
+                      faqData?.map(
+                        (item) =>
+                          item?.id === activeFaq &&
+                          item?.content?.map((content, index) => (
+                            <div key={index} className="question">
+                              <Collapse
+                                defaultActiveKey={["0"]}
+                                ghost
+                                expandIconPosition="right"
                                 style={{
-                                  borderRadius: "5px",
-                                  border: "1px solid #f0f0f0",
-                                  padding: "1em",
-                                  marginBottom: "1em",
+                                  fontSize: "1.15em",
                                 }}
                               >
-                                <p
-                                  dangerouslySetInnerHTML={{
-                                    __html: content?.answer,
+                                <Collapse.Panel
+                                  header={content?.question}
+                                  key={index}
+                                  style={{
+                                    borderRadius: "5px",
+                                    border: "1px solid #f0f0f0",
+                                    padding: "1em",
+                                    marginBottom: "1em",
                                   }}
-                                />
-                              </Collapse.Panel>
-                            </Collapse>
-                          </div>
-                        ))
-                    )}
-                </div>
-              </>
-            )}
+                                >
+                                  <p
+                                    dangerouslySetInnerHTML={{
+                                      __html: content?.answer,
+                                    }}
+                                  />
+                                </Collapse.Panel>
+                              </Collapse>
+                            </div>
+                          ))
+                      )}
+                  </div>
+                </>
+              )}
+            </div>
           </div>
-        </div>
-      </div>
+        </Content>
+      </Layout>
     </div>
   );
 };
