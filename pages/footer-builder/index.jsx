2a74e3f459e7aba94714d9d1d403c9e7d1a4edf5
atiqisrak <atiqisrak@gmail.com>
2024-10-25 20:24:18 +0600

    initiated footer builder for later

---
+++ pages/footer-builder/index.jsx
@@ -0,0 +1,13 @@
+// pages/footer-builder.jsx
+
+import React from "react";
+
+const FooterBuilderPage = () => {
+  return (
+    <div className="container mx-auto p-8">
+      <h1 className="text-2xl font-bold mb-4">Footer Builder</h1>
+    </div>
+  );
+};
+
+export default FooterBuilderPage;
